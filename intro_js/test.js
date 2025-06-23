217daec1100f0a09f4bd2cd9d53829a150016f26
Fabricio Nathan <fabricionathan749@gmail.com>
2025-06-23 18:24:30 -0300

    inicio de js, revisando a sintaxe

---
+++ intro_js/test.js
@@ -0,0 +1,11 @@
+// revisando a sintaxe de js
+
+console.log(1+1)
+console.log("testando string no js")
+console.log("Vai" + " " + "Corinthias")
+console.log(`a soma dos numeros sao =  ${2+2}`)
+let nome="Nafax"
+const idade=19
+console.log(`meu nome é ${nome} e minha idade é ${idade}`)
+const num=prompt("Digite um numero:")
+console.log(num)
