a3a5d1778d0fd013929339f01050e87104eedbf4
Fabricio Nathan <fabricionathan749@gmail.com>
2025-06-29 15:11:59 -0300

    treinando sintaxe js e ts

---
+++ intro_js/test2.js
@@ -0,0 +1,9 @@
+//add numero em vetor
+let pergun=Number(prompt("informe quantas vezes quer add um numero no vetor"));
+let numeros2=[]
+for(let i=0;i<pergun;i++){
+    const per=(prompt("informe um numero"))
+    numeros2[i]=per
+    console.log(numeros2[i])
+}
+
---
+++ intro_ts/prt.ts
@@ -0,0 +1,4 @@
+let numeros:number[]=[1,2,3]
+for(let i=0;i<numeros.length;i++){
+    console.log(numeros[i]);
+}
