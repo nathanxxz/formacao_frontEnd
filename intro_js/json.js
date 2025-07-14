d0afb1324dffac167b52f404f57cf01cc306f0de
Fabricio Nathan <fabricionathan749@gmail.com>
2025-07-14 10:46:42 -0300

    atualizacao do meu progresso

---
+++ intro_js/json.js
@@ -0,0 +1,13 @@
+
+const nome=prompt("Informe seu nome");
+try{
+    if(nome!="luffy"){
+        console.log("mentiroso");
+    }
+    else{
+        console.log("é o luffy");
+    }
+}catch(error){
+    console.log("erro no programa");
+    alert
+}
